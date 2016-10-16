685add81e4265dc53fb3aea2d76b6bb70b2a1662
Dmitry Ivanov <dmitry.ivanov.zagranju@gmail.com>
2016-10-16 15:25:58 +0200

    create scene, item (rect) and view

--- Test_Project/Test/main.cpp
+++ Test_Project/Test/main.cpp
@@ -6,10 +6,21 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    
-    //cokolwiek
+    QApplication a(argc, argv);    
 
+    // create a scene
+    QGraphicsScene * scene = new QGraphicsScene();
 
+    // create an item to put into the scene
+    QGraphicsRectItem * rect = new QGraphicsRectItem();
+    rect->setRect(0,0,100,100);
+
+    // add the item to the theme
+    scene->addItem(rect);
+
+    // add a view
+    QGraphicsView * view = new QGraphicsView(scene);
+
+    view->show();
     return a.exec();
 }
