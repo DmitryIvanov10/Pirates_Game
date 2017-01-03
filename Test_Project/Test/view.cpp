6dc5b690143f00ec4dacae5085fc3fff4757c514
Pitt <pitrol@windowslive.com>
2017-01-03 20:18:54 +0100

    changes in view

--- Test_Project/Test/view.cpp
+++ Test_Project/Test/view.cpp
@@ -2,15 +2,32 @@
 
 View::View() :QGraphicsView::QGraphicsView()
 {
-    //setMouseTracking(true);
+    setMouseTracking(true);
 }
 
 View::View(QGraphicsScene *scene) : QGraphicsView::QGraphicsView(scene)
 {
-    //setMouseTracking(true);
+    setMouseTracking(true);
+}
+
+View::View(QGraphicsScene *scene, int _res_x, int _res_y) : QGraphicsView::QGraphicsView(scene)
+{
+    setMouseTracking(true);
+    res_x = _res_x;
+    res_y = _res_y;
+}
+
+void View::mouseMoveEvent(QMouseEvent *event)
+{
+    //qDebug() << event->x() << " " << event->y();
 }
 
 void View::mousePressEvent(QMouseEvent *event)
 {
-    qDebug() << "jej :D";
+    qDebug() << "wciskam " << event->x() << " " << event->y();
+}
+
+void View::mouseReleaseEvent(QMouseEvent *event)
+{
+    qDebug() << "puszczam";
 }
