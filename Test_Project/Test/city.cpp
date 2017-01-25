1adc79c7c997a98a54ba19800fefdb51d3cb411b
Pitt <pitrol@windowslive.com>
2017-01-25 21:48:34 +0100

    ship methods in city class

--- Test_Project/Test/city.cpp
+++ Test_Project/Test/city.cpp
@@ -10,6 +10,11 @@ City::City(short _id_voronoi, short _fraction, short _id)
     fraction = _fraction;
     id = _id;
     active = false;
+
+    ship_models.push_back(3);
+    ship_prices.push_back(6500);
+    ship_models.push_back(4);
+    ship_prices.push_back(8000);
 }
 
 short City::show_fraction()
@@ -20,6 +25,20 @@ short City::show_fraction()
 float City::show_price(short _good)
 {
     return prices[_good];
+}
+
+short City::get_selling_ship_model(int number)
+{
+    if(number < 0 || number > ship_models.size())
+        return -1;
+    return ship_models[number];
+}
+
+int City::get_selling_ship_price(int number)
+{
+    if(number < 0 || number > ship_prices.size())
+        return -1;
+    return ship_prices[number];
 }
 
 void City::set_state(bool state)
